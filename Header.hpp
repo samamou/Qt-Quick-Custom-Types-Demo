2c0d3e69ae97d94db52a06b3e2e80cc8ed48c365
samamou <s_almam@live.concordia.ca>
2024-02-28 16:04:41 -0500

    added comparison operator methods

--- Header.hpp
+++ Header.hpp
@@ -3,8 +3,6 @@
 #include <QtQml>
 #include <compare>
 
-
-
 struct Mmpose_params{
     Q_GADGET
 
@@ -73,7 +71,6 @@ public:
     int display_delay = 0;
     bool synchronous_mode = false;
 
-    
     bool operator==(const Mmpose_params& other) const
     {
         // Implement equality comparison
@@ -97,10 +94,26 @@ struct Size {
 public:
     int width = 0;
     int height = 0;
+
+    bool operator==(const Size& other) const noexcept = default;
+    bool operator!=(const Size& other) const noexcept = default;
+
 };
 
 //params object 
 struct Posenet_params {
+    Q_GADGET
+    Q_PROPERTY(QString base_model MEMBER base_model)
+    Q_PROPERTY(int device_id MEMBER device_id)
+    Q_PROPERTY(bool draw_bounding_boxes MEMBER draw_bounding_boxes)
+    Q_PROPERTY(bool draw_skeleton_edges MEMBER draw_skeleton_edges)
+    Q_PROPERTY(QVector<Size> input_resolution MEMBER input_resolution)
+    Q_PROPERTY(float min_keypoint_confidence MEMBER min_keypoint_confidence)
+    Q_PROPERTY(float min_pose_confidence MEMBER min_pose_confidence)
+    Q_PROPERTY(bool multi_pose_detection MEMBER multi_pose_detection)
+    Q_PROPERTY(int output_stride MEMBER output_stride)
+
+public:
     QString base_model;
     int device_id =0;
     bool draw_bounding_boxes = false;
@@ -110,6 +123,9 @@ struct Posenet_params {
     float min_pose_confidence = 0.0f;
     bool multi_pose_detection = false;
     int output_stride = 0;
+
+    bool operator==(const Posenet_params& other) const noexcept = default;
+    bool operator!=(const Posenet_params& other) const noexcept = default;
 };
 
 struct Trt_params {
@@ -134,67 +150,144 @@ public:
     int model_height = 0;
     int model_width = 0;
     float min_pose_confidence = 0.0f;
+
+    bool operator==(const Trt_params& other) const noexcept = default;
+    bool operator!=(const Trt_params& other) const noexcept = default;
 };
 
 
 //pose_backends array of type variant
 struct Pose_backend {
+    Q_GADGET
+    Q_PROPERTY(QString name MEMBER name)
+    Q_PROPERTY(QVariant params MEMBER params)
+
+public:
     QString name;
     QVariant params; //either of the three types
+
+    bool operator==(const Pose_backend& other) const noexcept = default;
+    bool operator!=(const Pose_backend& other) const noexcept = default;
 };
 
 //camera parameters
 struct Camera_params {
+    Q_GADGET
+    Q_PROPERTY(Size resolution MEMBER resolution)
+    Q_PROPERTY(int framerate MEMBER framerate)
+    Q_PROPERTY(bool flip MEMBER flip)
+    Q_PROPERTY(int rotate MEMBER rotate)
+
+public:
     Size resolution = {0, 0};
     int framerate = 0;
     bool flip = false;
     int rotate = 0;
+
+    bool operator==(const Camera_params& other) const noexcept = default;
+    bool operator!=(const Camera_params& other) const noexcept = default;
 };
 
 struct Cameras {
+    Q_GADGET
+    Q_PROPERTY(QVector<QString> input_paths MEMBER input_paths)
+    Q_PROPERTY(bool flip_camera MEMBER flip_camera)
+    Q_PROPERTY(QMap<QString, Camera_params> params MEMBER params)
+    Q_PROPERTY(QMap<QString, QString> intrinsics MEMBER intrinsics)
+
+public:
     QVector<QString> input_paths;
     bool flip_camera = false;
     QMap<QString, Camera_params> params;
     QMap<QString, QString> intrinsics; //idk what the structure is here
+
+    bool operator==(const Cameras& other) const noexcept = default;
+    bool operator!=(const Cameras& other) const noexcept = default;
 };
 
 //dimmaps 
 struct Pose2DToFloor {
+    Q_GADGET
+    Q_PROPERTY(QMap<QString, QVector<QVector<float>>> pairs_per_camera MEMBER pairs_per_camera)
+
+public:
     QMap<QString, QVector<QVector<float>>> pairs_per_camera;
+
+    bool operator==(const Pose2DToFloor& other) const noexcept = default;
+    bool operator!=(const Pose2DToFloor& other) const noexcept = default;
 };
 
 struct Dimmaps {
+    Q_GADGET
+    Q_PROPERTY(Pose2DToFloor pose2d_to_floor MEMBER pose2d_to_floor)
+
+public:
     Pose2DToFloor pose2d_to_floor;
+
+    bool operator==(const Dimmaps& other) const noexcept = default; 
+    bool operator!=(const Dimmaps& other) const noexcept = default;
 };
 
 //skeleton obj
 struct SkeletonFilters {
+    Q_GADGET
+    Q_PROPERTY(bool two_dimensional MEMBER two_dimensional)
+    Q_PROPERTY(bool three_dimensional MEMBER three_dimensional)
+    Q_PROPERTY(float min_pose_completeness MEMBER min_pose_completeness)
+
+public:
     bool two_dimensional = false;
     bool three_dimensional = false;
     float min_pose_completeness = 0.0f;
+
+    bool operator==(const SkeletonFilters& other) const noexcept = default;
+    bool operator!=(const SkeletonFilters& other) const noexcept = default;
 };
 
 struct Filters {
+    Q_GADGET
+    Q_PROPERTY(SkeletonFilters skeletons MEMBER skeletons)
+
+public:
     SkeletonFilters skeletons;
+
+    bool operator==(const Filters& other) const noexcept = default;
+    bool operator!=(const Filters& other) const noexcept = default;
 };
 
 //osc output obj
 struct OscOutput {
+    Q_GADGET
+    Q_PROPERTY(QMap<QString, int> destinations MEMBER destinations)
+
+public:
     QMap<QString, int> destinations;
+
+    bool operator==(const OscOutput& other) const noexcept = default;
+    bool operator!=(const OscOutput& other) const noexcept = default;
 };
 
 //represents the entire config file obj
 struct Config {
+    Q_GADGET
+    Q_PROPERTY(QVector<Posenet_params> pose_backends MEMBER pose_backends)
+    Q_PROPERTY(QMap<QString, Camera_params> cameras MEMBER cameras)
+    Q_PROPERTY(SkeletonFilters filters MEMBER filters)
+    Q_PROPERTY(OscOutput outputs MEMBER outputs)
+
+public:
     QVector<Posenet_params> pose_backends;
     QMap<QString, Camera_params> cameras;
     SkeletonFilters filters;
     OscOutput outputs;
+
+    bool operator==(const Config& other) const noexcept = default;
+    bool operator!=(const Config& other) const noexcept = default;
 };
 
 struct DataModel : public QObject
 {
     Q_OBJECT // Macro to enable Qt's meta-object system for this class
-
     Q_PROPERTY(Mmpose_params data MEMBER data) // Declares a property named "data" with a member variable "data" of type MyObject
 
 public:
