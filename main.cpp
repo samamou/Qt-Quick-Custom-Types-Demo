a6e7867803c5a1fa326f10d7eb86e86d4cc84f18
samamou <46803937+samamou@users.noreply.github.com>
2024-03-14 17:35:30 -0400

    implementing data structs & exposing to qml  (#1)
    * first commit
    
    * added comparison operator methods
    
    * JSON output
    
    * update less than operator definition
    
    * removed negation operators
    
    * added pair of points
    
    * added new structs and parameters
    
    * added position2d and hubs structs
    
    * added TriangulateFilter and DistortionCoefficients
    
    * added rest of structs
    
    * config output works
    
    * update qml to have a working config output
    
    * Update setting data in qml
    
    * cameras fix
    
    * assigned skeleton filter
    
    * fixed qml UI (a bit)
    
    ---------
    
    Co-authored-by: Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>

--- main.cpp
+++ main.cpp
@@ -2,26 +2,34 @@
 #include <QQmlApplicationEngine>
 #include "Header.hpp"
 
+/**
+ * @brief The main function of the application.
+ *
+ * This function initializes the application, registers custom types, loads the QML file,
+ * and starts the event loop.
+ *
+ * @param argc The number of command line arguments.
+ * @param argv The command line arguments.
+ * @return The exit code of the application.
+ */
+
 int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
 
     QQmlApplicationEngine engine;
-
-    // Register custom types for QML
-    qmlRegisterType<MyObject>("com.myobject", 1, 1, "myObject"); // registers a custom class / type for QML 
+    // qmlRegisterType<Mmpose_params>("com.myobject", 1, 1, "mmpose_params"); // registers a custom class / type for QML 
     qmlRegisterType<DataModel>("com.myobject", 1, 1, "DataModel"); // registers a custom class / type for QML
 
+    
     const QUrl url(u"qrc:/test/Main.qml"_qs);
 
-    // Handle object creation failure
     QObject::connect(
         &engine,
         &QQmlApplicationEngine::objectCreationFailed,
         &app,
         []() { QCoreApplication::exit(-1); },
         Qt::QueuedConnection);
-
     engine.load(url);
 
     return app.exec();
